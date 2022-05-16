c0edb40e0749ab0418791e7e24b14830f97689ea
John-Thailand <hamoban69@gmail.com>
2022-05-16 15:55:01 +0900

    Unary RPC / Server Streaming RPC

---
+++ client/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"grpc-lesson/pb"
+	"io"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// サーバーとの接続
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewFileServiceClient(conn)
+	// callListFiles(client)
+	callDownload(client)
+}
+
+func callListFiles(client pb.FileServiceClient) {
+	res, err := client.ListFiles(context.Background(), &pb.ListFilesRequest{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(res.GetFileNames())
+}
+
+func callDownload(client pb.FileServiceClient) {
+	req := &pb.DownloadRequest{Filename: "name.txt"}
+	stream, err := client.Download(context.Background(), req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Printf("Response from Download(bytes): %v", res.GetData())
+		log.Printf("Response from Download(bytes): %v", string(res.GetData()))
+	}
+}
